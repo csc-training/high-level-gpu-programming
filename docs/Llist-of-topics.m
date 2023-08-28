c4ed95cb8698ff1d44e2aea4dc3cab8c773b8f43
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2023-08-28 15:05:31 +0300

    Create Llist-of-topics.m

---
+++ docs/Llist-of-topics.m
@@ -0,0 +1,27 @@
+## Introduction to GPU programming model
+## cpp concepts
+- structures
+- templates
+- lambdas
+- functors
+- smart pointers
+## SYCL
+- queues:
+    - device association
+    - device info
+    - behaivior
+- expressing parallelism:
+    - lambdas
+    - range
+    - nd_range
+- memory:
+    - buffers
+    - USM
+    - local share memory
+- dependencies:
+    -
+- internal profiling and debugging:
+    
+- cuda to sycl:
+- 
+## KOKKOS
