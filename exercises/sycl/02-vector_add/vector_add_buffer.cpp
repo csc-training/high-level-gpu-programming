a277801dce01d0143f4bc34343f30c57a335c751
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2024-01-17 11:05:41 +0200

    Create vector_add_buffer.cpp

---
+++ exercises/sycl/02-vector_add/vector_add_buffer.cpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2023 Intel Corporation
+
+// SPDX-License-Identifier: MIT
+#include <iostream>
+#include <sycl/sycl.hpp>
+using namespace sycl;
+
+int main() {
+  // Set up queue on any available device
+  queue q;
+
+  // Initialize input and output memory on the host
+  constexpr size_t N = 256;
+  std::vector<int> a(N), b(N), c(N);
+  std::fill(a.begin(), a.end(), 1);
+  std::fill(b.begin(), b.end(), 2);
+  std::fill(c.begin(), c.end(), 0);
+
+  {
+  // Create buffers for data and sum
+    buffer<int, 1> a_buf(a.data(), range<1>(N));
+    buffer<int, 1> b_buf(b.data(), range<1>(N));
+    buffer<int, 1> c_buf(c.data(), range<1>(N));
+
+    // Submit the kernel to the queue
+    q.submit([&](handler& h) {
+      accessor a{a_buf, h};
+      accessor b{b_buf, h};
+      accessor c{c_buf, h};
+
+      // BEGIN CODE SNIP
+      h.parallel_for(range{N}, [=](id<1> idx) {
+        c[idx] = a[idx] + b[idx];
+      });
+      // END CODE SNIP
+    });
+  }
+
+  // Check that all outputs match expected value
+  bool passed = std::all_of(c.begin(), c.end(),
+                            [](int i) { return (i == 3); });
+  std::cout << ((passed) ? "SUCCESS" : "FAILURE")
+            << std::endl;
+  return (passed) ? 0 : 1;
+}
