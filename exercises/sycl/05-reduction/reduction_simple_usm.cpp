10dc11ef5ec4b373d4458399cd4b2618cc39ca42
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2024-01-30 14:50:53 +0200

    Update reduction_simple_usm.cpp

--- exercises/sycl/05-reduction/reduction_simple_usm.cpp
+++ exercises/sycl/05-reduction/reduction_simple_usm.cpp
@@ -1,33 +1,38 @@
 // We use built-in sycl::reduction mechanism in this example.
-// The manual implementation of the reduction kernel can be found in folder 06-reduction
+// The manual implementation of the reduction kernel can be found in folder 04-reduction
 #include <sycl/sycl.hpp>
+
+using namespace sycl;
 
 int main() {
   queue q;
   unsigned n = 10;
 
+  // Allocate memory for sum using malloc_shared
+  int   *sum = malloc_shared<int>(1, q);
+
   // Initialize sum
-  int sum = 0;
-  {
-    // Create a buffer for sum to get the reduction results
-    buffer<int> sum_buf{&sum, 1};
+   *sum = 0;
 
-    // Submit a SYCL kernel into a queue
-    q.submit([&](handler &cgh) {
-      // Create temporary object describing variables with reduction semantics
-      auto sum_acc = sum_buf.get_access<access_mode::read_write>(cgh);
-      // We can use built-in reduction primitive
-      auto sum_reduction = reduction(sum_acc, plus<int>());
+  // Submit a SYCL kernel into a queue
+  q.submit([&](handler &cgh) {
+    // Create temporary object describing variables with reduction semantics
+    auto sum_acc = reduction(sum, plus<int>());
+    
+    // A reference to the reducer is passed to the lambda
+    cgh.parallel_for(range<1>{n}, sum_acc,
+                    [=](id<1> idx, auto &reducer) { reducer.combine(idx[0]); });
+  }).wait();
 
-      // A reference to the reducer is passed to the lambda
-      cgh.parallel_for(range<1>{n}, sum_reduction,
-                      [=](id<1> idx, auto &reducer) { reducer.combine(idx[0]); });
-    }).wait();
-    // The contents of sum_buf are copied back to sum by the destructor of sum_buf
-  }
+  // The contents of sum_usm are already available in host memory
+
   // Print results
-
-  std::cout << "sum = " << sum << "\n";
-  bool passed = (sum == (((n-1) * n) / 2));
+  std::cout << "sum = " << *sum << "\n";
+  bool passed = (*sum == (((n-1) * n) / 2));
   std::cout << ((passed) ? "SUCCESS" : "FAILURE") << "\n";
+
+  // Free the allocated memory
+  free(sum, q);
+  
+  return 0;
 }
