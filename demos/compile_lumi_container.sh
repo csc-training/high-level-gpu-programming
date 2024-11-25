d9a4a87be7f80678d6479cf9d96dc9f3170c8d40
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-11-25 15:18:42 +0200

    Rename LUMI scripts by compiler environment

--- demos/compile_lumi_container.sh
+++ demos/compile_lumi_container.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+[[ -z "$CONTAINER_EXEC" ]] || echo "CONTAINER_EXEC='$CONTAINER_EXEC'"
+
 args="$@"
+
 $CONTAINER_EXEC hipcc $args -O3 -std=c++20 --hipstdpar --hipstdpar-path=$HIPSTDPAR_PATH --offload-arch=gfx90a:xnack+ daxpy_stdpar.cpp -o stdpar.x
-$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a:xnack+ -Wno-unused-result daxpy_cuda_hip.cpp -o cuda_hip.x
-$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a:xnack+ -Wno-unused-result -DSYNC daxpy_cuda_hip.cpp -o cuda_hip_sync.x
-$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a:xnack+ -Wno-unused-result daxpy_blas.cpp -lhipblas -o blas.x
-$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a:xnack+ -Wno-unused-result -DSYNC daxpy_blas.cpp -lhipblas -o blas_sync.x
+$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result daxpy_cuda_hip.cpp -o cuda_hip.x
+$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result -DSYNC daxpy_cuda_hip.cpp -o cuda_hip_sync.x
+$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result daxpy_blas.cpp -lhipblas -o blas.x
+$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result -DSYNC daxpy_blas.cpp -lhipblas -o blas_sync.x
 
---
+++ demos/compile_lumi_oneapi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+args="$@"
+
+icpx $args -fuse-ld=lld -std=c++20 -O3 -fsycl -fsycl-pstl-offload=gpu -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx90a:xnack+ daxpy_stdpar.cpp -o stdpar_oneapi.x
+icpx $args -fuse-ld=lld -std=c++20 -O3 -fsycl                         -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx90a daxpy_sycl.cpp -o sycl_oneapi.x
+icpx $args -fuse-ld=lld -std=c++20 -O3 -fsycl                         -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx90a -DSYNC daxpy_sycl.cpp -o sycl_sync_oneapi.x
