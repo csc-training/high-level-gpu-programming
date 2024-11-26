c18cefaf06bae54870b33a4958d414f5abdfe569
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2024-11-26 13:27:00 +0200

    Cleaned up Kokkos hello

--- exercises/kokkos/01-hello/hello.cpp
+++ exercises/kokkos/01-hello/hello.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+
   Kokkos::initialize(argc, argv);
+
   std::cout << "Execution Space: " <<
-    typeid(Kokkos::DefaultExecutionSpace).name() << std::endl;
+    Kokkos::DefaultExecutionSpace::name() << std::endl;
   std::cout << "Memory Space: " <<
-    typeid(Kokkos::DefaultExecutionSpace::memory_space).name() << std::endl;
+    Kokkos::DefaultExecutionSpace::memory_space::name() << std::endl;
+
   Kokkos::finalize();
+
   return 0;
 }
