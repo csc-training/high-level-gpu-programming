c6f383df4699edc5f1634805bd824c113c06dd69
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-11-26 16:14:07 +0200

    Remove container exec

--- demos/compile_lumi.sh
+++ demos/compile_lumi.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-[[ -z "$CONTAINER_EXEC" ]] || echo "CONTAINER_EXEC='$CONTAINER_EXEC'"
-
 args="$@"
 
-$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result daxpy_cuda_hip.cpp -o cuda_hip.x
-$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result -DSYNC daxpy_cuda_hip.cpp -o cuda_hip_sync.x
-$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result daxpy_blas.cpp -lhipblas -o blas.x
-$CONTAINER_EXEC hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result -DSYNC daxpy_blas.cpp -lhipblas -o blas_sync.x
+hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result daxpy_cuda_hip.cpp -o cuda_hip.x
+hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result -DSYNC daxpy_cuda_hip.cpp -o cuda_hip_sync.x
+hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result daxpy_blas.cpp -lhipblas -o blas.x
+hipcc $args -O3 --offload-arch=gfx90a -Wno-unused-result -DSYNC daxpy_blas.cpp -lhipblas -o blas_sync.x
