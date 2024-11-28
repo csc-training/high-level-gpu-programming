a259aaa3eaaca026df8153f730558e53466377cd
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2024-11-28 14:12:26 +0200

    Update vector_add_buffer.cpp

--- exercises/sycl/02-vector_add/vector_add_buffer.cpp
+++ exercises/sycl/02-vector_add/vector_add_buffer.cpp
@@ -8,11 +8,10 @@ using namespace sycl;
 int main() {
   // Set up queue on any available device
   //TODO 
-  queue q;
 
   // Initialize input and output memory on the host
   constexpr size_t N = 256;
-  std::vector<int> a_host(N), b_host(N), c_host(N);
+  std::vector<int> a(N), b(N), c(N);
   std::fill(a.begin(), a.end(), 1);
   std::fill(b.begin(), b.end(), 2);
   std::fill(c.begin(), c.end(), 0);
@@ -29,6 +28,7 @@ int main() {
       h.parallel_for(
         //TODO
       );
+    });
 
       //TODO after the submission works
       //Checking inside the scope of the buffers
